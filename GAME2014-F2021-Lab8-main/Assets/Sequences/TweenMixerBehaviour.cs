1b93f1835029741c6b08994d9b589305bddcfae4
Vincent329 <vincee.ho@gmail.com>
2021-11-23 14:44:49 -0500

    [ADD] - Audio Track, Timeline animation

---
+++ GAME2014-F2021-Lab8-main/Assets/Sequences/TweenMixerBehaviour.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TweenMixerBehaviour : PlayableBehaviour
+{
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        base.ProcessFrame(playable, info, playerData);
+
+        
+        }
+}
